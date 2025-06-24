12af8d1132a24d3609f971e91d2ba15584b541ab
Dailing Wu <dailingwu04@gmail.com>
2025-06-24 12:18:14 -0400

    modified clock and power gating models to calculate using given dynamic and static power values

--- ULPmodels.py
+++ ULPmodels.py
@@ -89,18 +89,24 @@ class powerGating(ULPTechnique):
     Missing implementation of power and delay penalty during wake-up of the block.
     """
     def __init__(self,
-                 V_gs: float,   # Gate to source voltage
-                 W: float,      # Width of device
-                 L: float,      # Length of device
-                 mu_eff: float, # Effective mobility
-                 C_ox: float,   # Oxide capacitance of grid
-                 m: float,      # Slope coefficient of the current below sub-threshold,
-                 v_T: float,    # Thermal voltage kT/q
-                 V_th: float,   # Threshold voltage
-                 V_ds: float    # Drain to source voltage
+                 P_dyn: float,  # dynamic power of full block (W)
+                 P_stat: float,  # static (leakage) power of full block (W)
+                 frac_gated: float,  # fraction of block actually gated off
+                 Vdd_nom: float   # supply voltage (V)
+                 # V_gs: float,   # Gate to source voltage
+                 # W: float,      # Width of device
+                 # L: float,      # Length of device
+                 # mu_eff: float, # Effective mobility
+                 # C_ox: float,   # Oxide capacitance of grid
+                 # m: float,      # Slope coefficient of the current below sub-threshold,
+                 # v_T: float,    # Thermal voltage kT/q
+                 # V_th: float,   # Threshold voltage
+                 # V_ds: float    # Drain to source voltage
                  ):
         super().__init__()
-        self.I = (W/L) * mu_eff * C_ox * (m-1) * np.power(v_T, 2) * np.exp((V_gs - V_th) / (m * v_T)) * (1 - np.exp(-V_ds / v_T)) # Equivalent to subthreshold leakage current
+        #self.I = (W/L) * mu_eff * C_ox * (m-1) * np.power(v_T, 2) * np.exp((V_gs - V_th) / (m * v_T)) * (1 - np.exp(-V_ds / v_T)) # Equivalent to subthreshold leakage current
+        self.I = (P_stat * (1 - frac_gated) + P_dyn * (1 - frac_gated)) / Vdd_nom
+        # NOTE: There technically is some residual leakage while gated area is off. How is this calculated?
 
     def constraints(self):
         return super().constraints()
@@ -116,22 +122,29 @@ class clockGating(ULPTechnique):
     Missing implementation of power and delay penalty during wake-up of the block.
     """
     def __init__(self,
-                 V_gs: float,   # Gate to source voltage
-                 W: float,      # Width of device
-                 L: float,      # Length of device
-                 mu_eff: float, # Effective mobility
-                 C_ox: float,   # Oxide capacitance of grid
-                 m: float,      # Slope coefficient of the current below sub-threshold,
-                 v_T: float,    # Thermal voltage kT/q
-                 V_th: float,   # Threshold voltage
-                 V_ds: float,   # Drain to source voltage
-                 P_stat: float, # Static Power
-                 V_dd: float    # Power supply voltage
+                 P_dyn: float,  # dynamic power of full block (W)
+                 P_stat: float,  # static (leakage) power of full block (W)
+                 frac_gated: float,  # fraction of block that is clock-gated
+                 Vdd_nom: float   # supply voltage (V)
+                 # V_gs: float,   # Gate to source voltage
+                 # W: float,      # Width of device
+                 # L: float,      # Length of device
+                 # mu_eff: float, # Effective mobility
+                 # C_ox: float,   # Oxide capacitance of grid
+                 # m: float,      # Slope coefficient of the current below sub-threshold,
+                 # v_T: float,    # Thermal voltage kT/q
+                 # V_th: float,   # Threshold voltage
+                 # V_ds: float,   # Drain to source voltage
+                 # P_stat: float, # Static Power
+                 # V_dd: float    # Power supply voltage
                  ):
         super().__init__()
-        self.I_sub = (W/L) * mu_eff * C_ox * (m-1) * np.power(v_T, 2) * np.exp((V_gs - V_th) / (m * v_T)) * (1 - np.exp(-V_ds / v_T))
-        self.I_idle = P_stat / V_dd
-        self.I = self.I_sub + self.I_idle
+        #self.I_sub = (W/L) * mu_eff * C_ox * (m-1) * np.power(v_T, 2) * np.exp((V_gs - V_th) / (m * v_T)) * (1 - np.exp(-V_ds / v_T))
+        #self.I_idle = P_stat / V_dd
+        #self.I = self.I_sub + self.I_idle
+        I_leak = P_stat / Vdd_nom
+        I_dyn  = (P_dyn * (1 - frac_gated)) / Vdd_nom
+        self.I = I_leak + I_dyn
     
     def constraints(self):
         return super().constraints()
@@ -150,6 +163,9 @@ class DVFS(ULPTechnique):
     Phase 5: Idle
     Phase 1 and 4 can have lower frequency/voltage. Phase 2 and 3 should have higher frequency and voltage to finish the task. Phase 5 should have lowest freq./voltage.
     For now, the user sets the voltage and frequency limits. In the future, perhaps the class can integrate the freq./volt. limits internally.
+
+    Typically, there are a number of set frequencies that DVFS can toggle between to optimize for power and performance.
+    This model should probably have the option for a list of frequencies to choose from and calculate power loss for each frequency state.
     """
     def __init__(self,
                  phase: int,
